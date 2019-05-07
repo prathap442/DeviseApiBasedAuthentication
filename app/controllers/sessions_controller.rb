5fe6cd57c0f158c4102bfa725dabec70bf45c89e
prathap <prathap.mohan@kloctechnologies.co.in>
2019-05-07 10:50:17 +0530

    made changes in few of the files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@ class SessionsController < Devise::SessionsController
     binding.pry
     super
     binding.pry
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    binding.pry
+    yield resource if block_given?
+    respond_with resource do |format|
+      format.json { render json: {message: "signed in successfully",url: after_sign_in_path_for(resource)}} 
+    end
   end
 end
